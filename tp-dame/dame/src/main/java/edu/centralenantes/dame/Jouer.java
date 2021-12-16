efac8b8283481ef6c67d2d2969ea626ee1151479
AlexandreBodet <bodetalexandre1999@gmail.com>
2021-12-16 11:30:52 +0100

    debut main

--- tp-dame/dame/src/main/java/edu/centralenantes/dame/Jouer.java
+++ tp-dame/dame/src/main/java/edu/centralenantes/dame/Jouer.java
@@ -5,10 +5,68 @@
  */
 package edu.centralenantes.dame;
 
+import java.util.Scanner;
+
 /**
  *
  * @author uble
  */
 public class Jouer {
    
+    public static void main(String[] args) {
+        
+        Plateau plat = new Plateau();
+        
+        System.out.println("Bienvenue au jeu de dames. Plateau crééé");
+        
+        while (true){
+            tourDeJeu(plat);
+            
+            
+        }
+        
+        
+    }
+    
+    /**
+     *
+     * @param pla plateau de jeu
+     */
+    public static void tourDeJeu(Plateau pla){
+        Scanner keyboard = new Scanner(System.in);
+        String answer;
+        System.out.println("Tour du Joueur 1");
+        
+        System.out.println("Souhaitez-vous déplacer ou prendre un pion? d/p");
+        do {
+            answer = keyboard.nextLine();
+        } while (!answer.equals("d") && !answer.equals("p"));
+        
+        if ("d".equals(answer)){
+            System.out.println("Coordonnées du pion à déplacer? ");
+            int x;
+            int y;
+            
+            while (true){
+                System.out.println("x ?");
+                x = keyboard.nextInt();
+                
+                System.out.println("y ?");
+                y= keyboard.nextInt();
+                
+                if (x<10 && x>=0 && y<10 && y>=0){
+                    break;  // sort de la boucle si les valeurs sont bonnes
+                }
+                
+                
+            }
+            
+            ;
+            
+            
+        }
+        
+        
+    }
+    
 }
